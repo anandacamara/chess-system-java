d7edd5e487f30fc7f17077c87d219747deead65d
Ananda Câmara <ananda.lr.camara@gmail.com>
2020-07-10 19:49:46 -0300

    First Class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,8 +6,9 @@ public class Program {
 
 	public static void main(String[] args) {
 	
-		Position pos = new Position(3, 5);
+		Position pos = new Position(2, 5);
 		System.out.println(pos);
+		
 	}
 
 }
